262a8afd06da35312db39f6b86c33038bc235b14
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2021-03-12 09:14:52 +0100

    make zx0-depacker configurable

--- bitfire/zx0/6502/dzx0.asm
+++ bitfire/zx0/6502/dzx0.asm
@@ -1,10 +1,12 @@
 !cpu 6510
 
-INPLACE		= 0
-.ZP_ADDR	= $f8
-.lz_dst		= .ZP_ADDR + 0
-.lz_bits	= .ZP_ADDR + 2
-.lz_len_hi	= .ZP_ADDR + 4
+ZX0_INPLACE		= 0
+ZX0_INLINE_GET_LEN	= 0
+
+.ZP_ADDR		= $f8
+.lz_dst			= .ZP_ADDR + 0
+.lz_bits		= .ZP_ADDR + 2
+.lz_len_hi		= .ZP_ADDR + 4
 
 .depacker_start
 		;------------------
@@ -23,7 +25,7 @@ INPLACE		= 0
 		iny
 		sty <.lz_len_hi
 		lda #$40
-		sta <.lz_bits			;will make us fall through on next test and force us to load a new byte into bit-buffer upon next .get_length
+		sta <.lz_bits			;will make us fall through on next test and force us to load a new byte into bit-buffer upon next .lz_get_len
 
 		;------------------
 		;LITERAL
@@ -31,14 +33,29 @@ INPLACE		= 0
 .lz_start_over
 		lda #$01
 		asl <.lz_bits
-		bcs .lz_new_offset		;after each match check for another match or literal
-.literal
-		jsr .get_length
+!if ZX0_INLINE_GET_LEN == 1 {
+		bcc .lz_literal
+		jmp .lz_new_offset		;after each match check for another match or literal
+-						;lz_get_len as inline
+		asl <.lz_bits			;fetch payload bit
+		rol
+.lz_literal
+		asl <.lz_bits
+		bcc -
++
+		bne +
+		jsr .lz_refill_bits
++
+} else {
+		bcs .lz_new_offset
+.lz_literal
+		jsr .lz_get_len
+}
 		sta .lz_y + 1
 		and #$ff			;annoying, but flags are not set corresponding to A
 		beq .lz_l_page
 ;		dec <.lz_len_hi			;happens very seldom, so let's do that with lz_l_page that also decrements lz_len_hi
-.cp_literal
+.lz_cp_literal
 .lz_src1 = * + 2
 		lda $1000,x
 		inx
@@ -48,7 +65,7 @@ INPLACE		= 0
 		sta (.lz_dst),y
 		iny
 .lz_y		cpy #$00
-		bne .cp_literal
+		bne .lz_cp_literal
 
 		dey				;this way we force increment of lz_dst + 1 if y = 0
 		tya				;carry is still set on first round
@@ -64,16 +81,29 @@ INPLACE		= 0
 		;NEW OR OLD OFFSET
 		;------------------
 
-.cp_literal_done
 		lda #$01
 		asl <.lz_bits
 		bcs .lz_new_offset		;either match with new offset or old offset
+!if ZX0_INLINE_GET_LEN == 1 {
+		bcc .lz_match_repeat
 
 		;------------------
 		;DO MATCH
 		;------------------
+-						;lz_get_len as inline
+		asl <.lz_bits			;fetch payload bit
+		rol
 .lz_match_repeat
-		jsr .get_length
+		asl <.lz_bits
+		bcc -
++
+		bne +
+		jsr .lz_refill_bits
++
+} else {
+.lz_match_repeat
+		jsr .lz_get_len
+}
 		sbc #$01			;saves the iny later on
 		sec				;need sec here if we want to forgo in the beq .lz_calc_msrc
 .lz_match_
@@ -93,16 +123,16 @@ INPLACE		= 0
 		lda <.lz_dst + 1
 .lz_offset_hi	adc #$ff
 		sta .lz_msrcr + 1
-.cp_match
+.lz_cp_match
 .lz_msrcr = * + 1
 		lda $beef,y
 		sta (.lz_dst),y
 		iny
-		bne .cp_match
+		bne .lz_cp_match
 		inc <.lz_dst + 1
 
 		lda <.lz_len_hi			;check for more loop runs
-!if INPLACE == 1 {
+!if ZX0_INPLACE == 1 {
 		bne .lz_m_page			;do more page runs
 
 		cpx <.lz_dst + 0		;check for end condition when depacking inplace
@@ -120,7 +150,7 @@ INPLACE		= 0
 .lz_m_page
 		dec <.lz_len_hi
 		inc .lz_msrcr + 1
-		bne .cp_match
+		bne .lz_cp_match
 .lz_clc
 		clc
 		bcc .lz_clc_back
@@ -128,12 +158,14 @@ INPLACE		= 0
 		dec <.lz_len_hi
 		sec				;only needs to be set for consecutive rounds of literals, happens very seldom
 		ldy #$00
-		beq .cp_literal
+		beq .lz_cp_literal
 
 		;------------------
 		;FETCH A NEW OFFSET
 		;------------------
--						;get_length as inline
+
+!if ZX0_INLINE_GET_LEN == 1 {
+-						;lz_get_len as inline
 		asl <.lz_bits			;fetch payload bit
 		rol
 .lz_new_offset
@@ -143,6 +175,10 @@ INPLACE		= 0
 		bne +
 		jsr .lz_refill_bits
 +
+} else {
+.lz_new_offset
+		jsr .lz_get_len
+}
 		sbc #$01
 		bcc .lz_eof			;underflow. must have been 0
 		eor #$ff
@@ -163,6 +199,7 @@ INPLACE		= 0
 		ldy #$fe
 		bcs .lz_match__			;length = 2 ^ $ff, do it the very short way :-)
 		ldy #$00
+!if ZX0_INLINE_GET_LEN == 1 {
 -
 		asl <.lz_bits			;fetch first payload bit
 		rol
@@ -170,6 +207,9 @@ INPLACE		= 0
 		bcc -
 		bne .lz_match_
 		jsr .lz_refill_bits		;fetch remaining bits
+} else {
+		jsr .lz_get_len_
+}
 		bcs .lz_match_
 
 .lz_inc_src_hi
@@ -178,13 +218,16 @@ INPLACE		= 0
 		inc .lz_src3
 		rts
 
-.get_loop
+!if ZX0_INLINE_GET_LEN == 0 {
+.lz_get_len_
+-						;lz_get_len as inline
 		asl <.lz_bits			;fetch payload bit
-		rol				;shift in 8 bits first
-.get_length
+		rol
+.lz_get_len
 		asl <.lz_bits
-		bcc .get_loop
-		bne .end_bit_16
+		bcc -
+		bne .lz_get_end
+}
 .lz_refill_bits					;refill bits, this happens after 4 payload-bits bestcase
 .lz_src3 = * + 2
 		ldy $1000,x
@@ -195,16 +238,16 @@ INPLACE		= 0
 		sty <.lz_bits
 		ldy #$00
 		rol <.lz_bits
-		bcs .end_bit_16
-.get_loop_16					;continue with 16 bit shifting
+		bcs .lz_get_end
+-						;continue with 16 bit shifting
 		asl <.lz_bits			;fetch payload bit
 		rol				;can also moved to front and executed once on start
-.get_length_16
+.lz_get_len_16
 		rol <.lz_len_hi
 		asl <.lz_bits
-		bcc .get_loop_16
+		bcc -
 		beq .lz_refill_bits
-.end_bit_16
+.lz_get_end
 .lz_eof
 		rts
 .depacker_end
