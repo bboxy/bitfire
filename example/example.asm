4c4bf9414323f76a1f89f5597da7022b08a86c74
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2021-04-03 15:48:58 +0200

    clean up example

--- example/example.asm
+++ example/example.asm
@@ -1,8 +1,8 @@
 !src "../loader/loader_acme.inc"
+!src "../macros/link_macros_acme.inc"
 
 		* = $1000
 !bin "../loader/installer",,2
-
 		* = $0800
 		lda #$0b
 		sta $d020
@@ -31,10 +31,58 @@
 		lda #$35
 		sta $01
 .loop
+		jsr .start_timer
 		lda #$00
-		jsr link_load_comp
-		jsr link_load_next_comp
+		jsr bitfire_loadcomp_
+		ldx #$00
+		jsr .stop_timer
+		jsr .start_timer
+		lda #$01
+		jsr bitfire_loadcomp_
+		ldx #$04
+		jsr .stop_timer
+		jsr .start_timer
 		lda #$02
-		jsr link_load_raw
-		jsr link_load_next_raw
+		jsr bitfire_loadraw_
+		ldx #$08
+		jsr .stop_timer
+		jsr .start_timer
+		lda #$03
+		jsr bitfire_loadraw_
+		ldx #$0c
+		jsr .stop_timer
 		jmp .loop
+.start_timer
+		lda #$00
+		sta $dc0e
+		lda #$40
+		sta $dc0f
+		lda #$ff
+		sta $dc04
+		sta $dc05
+		sta $dc06
+		sta $dc07
+		lda #$41
+		sta $dc0f
+		lda #$01
+		sta $dc0e
+		rts
+.stop_timer
+		lda #$00
+		sta $dc0e
+		lda #$40
+		sta $dc0f
+
+		lda $dc04
+		eor #$ff
+		sta $0f03,x
+		lda $dc05
+		eor #$ff
+		sta $0f02,x
+		lda $dc06
+		eor #$ff
+		sta $0f01,x
+		lda $dc07
+		eor #$ff
+		sta $0f00,x
+		rts
