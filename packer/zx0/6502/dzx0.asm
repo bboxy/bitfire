c496fcf915249cb24c658b470137b257ef39e9b6
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2021-05-31 09:40:39 +0200

    added warning

--- packer/zx0/6502/dzx0.asm
+++ packer/zx0/6502/dzx0.asm
@@ -1,3 +1,6 @@
+;/!\ Attention, this depacker only works with the original zx0 version contained within this folder was well, it does not work with the modified version coming with bitfire, as some things on the encoding got changed due to speed optimizations
+
+
 !cpu 6510
 
 ZX0_INPLACE		= 0
