8a8e97534cb2d0645dd4c4bfcf9302ea7a22b224
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2022-06-24 17:25:49 +0200

    solid loading times with THCMs floppy

--- loader/drivecode.asm
+++ loader/drivecode.asm
@@ -188,7 +188,7 @@
 !pseudopc .drivecode {
 .zp_start
 
-;.free			= .zp_start + $00
+.speedzone		= .zp_start + $00
 .max_sectors		= .zp_start + $08			;maximum sectors on current track
 .dir_sector		= .zp_start + $10
 .blocks_on_list		= .zp_start + $11			;blocks tagged on wanted list
@@ -291,6 +291,18 @@ ___			= $ff
 ;v = v-flag clear
 ;g = gcr slowdown
 
+
+;			lda $1c00
+;			and #$9f
+;			asl					;shift to right position
+;			asl
+;			asl
+;			asl					;0xx10000 -> $10, $30, $50, $70
+;			sbx #$01				;preserve led, motor and stepper-bits -> $0f, $2f, $4f, $6f -> 0xx01111
+;			lda $1c00
+;			ora #$60				;x11xxxxx new bitrate bits to be set
+;			sax $1c00				;merge
+
 .read_loop
 			ldx #$3e
 			lda $1c01				;22333334
@@ -385,62 +397,65 @@ ___			= $ff
 
 ;bit rate   0         10        20        30        40        50        60        70        80        90        100       110       120       130       140       150       160
 ;0          1111111111111111111111111111111122222222222222222222222222222222333333333333333333333333333333334444444444444444444444444444444455555555555555555555555555555555
-;                1                      ccccccccccc   2                   ggggggggggggg...3ggggggggggg                 ccccccc   4             v    5         bbbbbbbbbbbbbb
+;              1                      ccccccccccc   2                   ggggggggggggg...3ggg                 cccccccggg   4ggggg               v      5       bbbbbbbbbbbbbb
 ;1          111111111111111111111111111111222222222222222222222222222222333333333333333333333333333333444444444444444444444444444444555555555555555555555555555555
-;                1                      ccccccccccc   2                   ggggggg...3ggggggggg                 ccccccc   4             v    5         bbbbbbbbbbbb
+;              1                      ccccccccccc   2                   ggggg...3ggg                 cccccccggg   4ggggg               v      5       bbbbbbbbbbbb
 ;2          11111111111111111111111111112222222222222222222222222222333333333333333333333333333344444444444444444444444444445555555555555555555555555555
-;                1                      ccccccccccc   2                   ggg...3ggggg                 ccccccc   4             v    5         bbbbbbbbbb
+;              1                      ccccccccccc   2                   ...3                 cccccccggg   4ggggg               v      5       bbbbbbbbbb
 ;3          1111111111111111111111111122222222222222222222222222333333333333333333333333334444444444444444444444444455555555555555555555555555
-;                1                      ccccccccccc   2                   ...3                 ccccccc   4             v    5         bbbbbbbb
+;              1                      ccccccccccc   2                   ...3                 ccccccc   4               v      5       bbbbbbbb
 
 .gcr_slow1_00
 			lsr $00
-			jmp +
+			jmp .gcr_slow1_20
 !if .GCR_125 = 1 {
 .tab0070dd77_hi
                         !byte                          $b0, $80, $a0, ___, $b0, $80, $a0, ___, $b0, $80, $a0
 }
-
-.gcr_slow1_40
-			lda $1c01
-			nop
---
-			jmp .gcr_slow1 + 3
 .gcr_slow1_20
 			nop
-			nop
 			lda $1c01
-			ldy $01
--
-			jmp --
-
+			jmp .gcr_slow1 + 3
+.gcr_slow2_xx
+			lax $1c01
 			nop
+			jmp .gcr_slow2 + 3
+.gcr_slow2_00
+.gcr_slow2_20
+.gcr_slow2_40
+			lax $1c01
 			nop
-			nop
-			nop
+			jmp .gcr_slow2 + 3
 
 !if .GCR_125 = 1 {
                         !byte                          $20, $00, $80, ___, $20, $00, $80, ___, $20, $00, $80
 }
 
-+
-			nop
-			lda $1c01
-			nop
-			jmp -
-
 .slow_tab1
 			!byte $4c
 			!byte $4c
-			!byte $4c
+			!byte $ad
 			!byte $ad
 			!byte <.gcr_slow1_00
 			!byte <.gcr_slow1_20
-			!byte <.gcr_slow1_40
+			!byte $01
 			!byte $01
 			!byte >.gcr_slow1_00
 			!byte >.gcr_slow1_20
-			!byte >.gcr_slow1_40
+			!byte $1c
+			!byte $1c
+.slow_tab2
+			!byte $4c
+			!byte $4c
+			!byte $4c
+			!byte $af
+			!byte <.gcr_slow2_00
+			!byte <.gcr_slow2_20
+			!byte <.gcr_slow2_40
+			!byte $01
+			!byte >.gcr_slow2_00
+			!byte >.gcr_slow2_20
+			!byte >.gcr_slow2_40
 			!byte $1c
 
 			;----------------------------------------------------------------------------------------------------
@@ -962,12 +977,22 @@ ___			= $ff
 			ldy <.speedzone				;.speedzone
 }
 
+			ldx #$00
+			top
+-
+			ldx #.gcr_slow2 - .gcr_slow1
 			lda .slow_tab1 + 0,y
-			sta <.gcr_slow1 + 0			;modify single point in gcr_loop for speed adaptioon, lda $1c01 or branch out with a jmp to slow down things
+			sta <.gcr_slow1 + 0,x			;modify single point in gcr_loop for speed adaptioon, lda $1c01 or branch out with a jmp to slow down things
 			lda .slow_tab1 + 4,y
-			sta <.gcr_slow1 + 1
+			sta <.gcr_slow1 + 1,x
 			lda .slow_tab1 + 8,y
-			sta <.gcr_slow1 + 2
+			sta <.gcr_slow1 + 2,x
+			tya
+			;sec
+			adc #11
+			tay
+			txa
+			beq -
 
 			;----------------------------------------------------------------------------------------------------
 			;
@@ -1208,14 +1233,14 @@ ___			= $ff
 			lda #$0f
 			sbx #.CHECKSUM_CONST1			;4 bits of a trailing zero after checksum
 			bne .retry_no_count			;check remaining nibble if it is $05
-			ldx $1c01
-			clv					;after arr, as it influences v-flag
-!if <* == $ff {
-			!error "bvc * crosses page!"
-}
-			bvc *
-			cpx #.CHECKSUM_CONST2			;0 01010 01 - more traiing zeroes
-			bne .retry_no_count
+;			ldx $1c01
+;			clv					;after arr, as it influences v-flag
+;!if <* == $ff {
+;			!error "bvc * crosses page!"
+;}
+;			bvc *
+;			cpx #.CHECKSUM_CONST2			;0 01010 01 - more traiing zeroes
+;			bne .retry_no_count
 ;			lda $1c01
 ;			and #$e0
 ;			cmp #.CHECKSUM_CONST3 & $e0		;010 xxxxx - and more trailing zeroes, last nibble varies on real hardware
