d462c3e5e2d11a271f070479059c31db6acb8934
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2023-01-25 14:46:18 +0100

    update macro generation

--- macros/convert.sh
+++ macros/convert.sh
@@ -4,9 +4,9 @@ cat ./link_macros_acme.inc | \
 #comments \
 #sed 's/;/;/g' | \
 #remove arg
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\t\tmac \2/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\1\t\tmac \3/g' | \
 #!macro name { \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\t\tmac \2 /g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\1\t\tmac \3 /g' | \
 #add end \
 sed 's/}/endm/g' | \
 #replace arg within block \
@@ -19,9 +19,9 @@ cat ./link_macros_acme.inc | \
 #comments \
 #sed 's/;/;/g' | \
 #remove arg \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\2.macro/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\1\3.macro/g' | \
 #!macro name { \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\2.macro/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\1\3.macro/g' | \
 #add end \
 sed 's/[[:space:]]*}/.endm/g' | \
 #replace arg within block \
@@ -38,9 +38,9 @@ cat ./link_macros_acme.inc | \
 #replace .l \
 sed 's/\.l/l/g' | \
 #remove arg \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\t\t.macro \2 arg\n\t\t.local l/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\1\t\t.macro \3 arg\n\t\t.local l/g' | \
 #!macro name { \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\t\t.macro \2\n\t\t.local l/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\1\t\t.macro \3\n\t\t.local l/g' | \
 #add end \
 sed 's/[[:space:]]*}/\t\t.endmacro/g' | \
 #replace arg within block \
@@ -48,30 +48,32 @@ sed 's/\.arg/arg/g' \
 > ./link_macros_ca65.inc
 ################### KICKASS ###################
 cat ./link_macros_acme.inc | \
-#comments \
-sed 's/;/\/\//g' | \
 sed 's/(/[/g' | \
 sed 's/)/]/g' | \
 #replace .l \
 sed 's/^\.l/l:/g' | \
+sed 's/^;\.l/;l:/g' | \
 sed 's/\.l/l/g' | \
 #remove arg \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/.macro \2(arg) {/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\1.macro \3(arg) {/g' | \
 #!macro name { \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/.macro \2() {/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\1.macro \3() {/g' | \
 #replace arg within block \
-sed 's/\.arg/arg/g' \
+sed 's/\.arg/arg/g' | \
+#comments \
+sed 's/;/\/\//g' \
 > ./link_macros_kickass.inc
 ################### DREAMASS ###################
 cat ./link_macros_acme.inc | \
 #comments \
 #replace .l \
 sed 's/^\.l/l:/g' | \
+sed 's/^;\.l/;l:/g' | \
 sed 's/\.l/l/g' | \
 #remove arg \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\t\t#macro \2(arg) {\n.(/g' | \
+sed 's/\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*\(\.arg\)[[:space:]]*{/\1\t\t#macro \3(arg) {\n.(/g' | \
 #!macro name { \
-sed 's/.*\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\t\t#macro \2\n\t\t{\n.(/g' | \
+sed 's/i\(.*\)\(!macro\)[[:space:]]*\(.*\)[[:space:]]*{/\1\t\t#macro \3\n\t\t{\n.(/g' | \
 #endmacro \
 sed 's/[[:space:]]*}/).\n\t\t}/g' | \
 #replace arg within block \
